5c3886db20278c977cd8b3f17af74826a78f20d4
tasyaufy08 <tasyaufy@gmail.com>
2024-11-29 12:51:34 +0700

    first commit

---
+++ resources/views/view_route.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>View Route</title>
+    @vite('resources/sass/app.scss')
+</head>
+<body>
+    <div class="container m-5">
+        <h1>Mari belajar laravel Routing</h1>
+     <div class="list-group mt-4">
+        <a href="{{ url('/company-profile') }}" class="list-group-item list-group-item-action">
+            Tugas (Bootstrap Clone)
+        </a>
+    </div>
+    @vite('resources/js/app.js')
+    </div>
+</body>
+</html>
